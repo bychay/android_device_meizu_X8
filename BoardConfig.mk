09da11b5b8f8496ca867e9fe6e9e1eddf523207e
bychay <94874469+bychay@users.noreply.github.com>
2025-09-06 16:50:37 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ BOARD_HAVE_QCOM_FM := true
 TARGET_SCREEN_DENSITY := 400
 
 # Kernel
-#TARGET_KERNEL_CONFIG += vendor/meizu/16.config
+#TARGET_KERNEL_CONFIG += vendor/meizu/X8.config
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
